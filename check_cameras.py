f1f6ac050641c95b3dbf8d6a57af914b2d5c7ead
JetsonIlya64GB <bestilyapetrov@gmail.com>
2023-05-25 11:04:23 +0200

    check cameras update

--- check_cameras.py
+++ check_cameras.py
@@ -3,6 +3,7 @@ import cv2
 import time
 import numpy as np
 import traceback
+import glob
 
 
 class vStream:
@@ -33,54 +34,51 @@ def main():
     try:
         w = 640
         h = 480
-        cam1 = vStream(0,w,h)
-        cam2 = vStream(1,w,h)
 
+        # Find all available video devices
+        devices = glob.glob('/dev/video*')
+        num_devices = len(devices)
+
+        if num_devices == 0:
+            print("No video devices found.")
+            return
+
+        print("Available video devices:")
+        for i, device in enumerate(devices):
+            print(f"{i+1}. {device}")
+
+        choice = input("Enter the number of the video device you want to use: ")
+        choice = int(choice)
+
+        if choice < 1 or choice > num_devices:
+            print("Invalid choice.")
+            return
+
+        # Subtracting 1 to match list indices
+        selected_device = devices[choice - 1][-1]
+
+        cam = vStream(selected_device, w, h)
 
         while True:
             try:
-                myFrame1 = cam1.getFrame()
-                myFrame2 = cam2.getFrame()
-                # cv2.imshow('Cam1', myFrame1)
-                # cv2.imshow('Cam2', myFrame2)
-                myFrame3 = np.hstack((myFrame1,myFrame2))
-                cv2.imshow('ComboCam', myFrame3)
-                # cv2.moveWindow('ComboCam',0,0)
+                myFrame = cam.getFrame()
+                cv2.imshow('Camera View', myFrame)
             except Exception as e:
-                print('frame unavailable')
-                print("Error is: ", e)
+                print('Frame unavailable')
+                print("Error:", e)
                 traceback.print_exc()
 
             if cv2.waitKey(1) == ord('q'):
-                cam1.capture.release()
-                cam2.capture.release()
+                cam.capture.release()
                 cv2.destroyAllWindows()
-                exit(1)
                 break
     except KeyboardInterrupt:
         print()
         print("Keyboard interrupt. Exiting peacefully")
-        cam1.capture.release()
-        # cam2.capture.release()
+        cam.capture.release()
         cv2.destroyAllWindows()
         exit(1)
 
 
 if __name__ == "__main__":
     main()
-
-
-# while True:
-#     cap = cv2.VideoCapture(0)
-    
-#     try:
-#         ret,frame = cap.read()
-#         cv2.imshow('Cam2', frame)
-#     except:
-#         print('bitch')
-
-#     if cv2.waitKey(1) == ord('q'):
-#         cap.release()
-#         cv2.destroyAllWindows()
-#         exit(1)
-#         break
